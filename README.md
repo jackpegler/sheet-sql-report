1d0c7785766aba597f569c2063db5b7fbce4507a
jackpegler <jackpegler@gmail.com>
2020-09-15 08:50:43 +0300

    added functions to update sql/gcloud connection info. Updated readme

--- README.md
+++ README.md
@@ -28,14 +28,39 @@ To install simply use `pip install sheet-to-sql`
 
 Then to use in your project simply import using `import sheet_sql_report`
 
-## How To:
+## Getting Started
 
 ### Configure connection to datasources
+To work with the **sheet-sql_report** package you first need to create a `SheetToSQL` object. You can initialise with connection info for Google and SQL - Both, either or neither; although the connection info is needed to run other methods.
+The `SheetToSQL()` object takes two optional arguments:
+
+* **google_cloud_credentials** which is a string with a path to your gcloud JSON credentails file
+* **sql_connection** a string of your database connection info as described above (*dialect+driver://username:password@host:port/database*)
+
+#### Updating connection info
+To update the connection info, either if you didn't add at the start or want to switch databases/google logins you can use:
+* `update_google_details()`
+* `update_sql_connection()`
 
 ### Grab data from google Sheet
+`get_gheet_data(workbook_name, sheet_name, cell=False)`
+* **worksheet_name**: the name of our Google Sheet that has the data we want
+* **sheet_name**: name of the sheet within the workbook we want to access
+* **cell**: (OPTIONAL) a sting with the cell to pull the data from e.g. 'A2' otherwise pulls whole sheet
 
 ### Grab data from sql
+`get_sql_data(query)`
+* **query**: a string containing the query to run on the database. Careful of certain characters that need to be different for Python e.g. % -> %%
 
 ### add data to google sheet
+`data_to_gsheet(df_data, workbook_name, sheet_name, starting_cell = 'A2')`
+* **df_data** (dataframe): the new data to add to the google sheet
+* **workbook_name** (string): the name of the workbook to update
+* **sheet_name** (string): the name of the worksheet to update
+* **starting_cell** (string): the cell location to input the new data. Defualt at A2
 
-### add data to sql 
+### add data to sql
+`data_to_sql(df, db_schema, db_table)`
+* **df (dataframe)**: data to feed to the database
+* **db_table (string)**: is name of the target table in the database
+* **db_schema (string)**: is name of the target schema in the database
--- sheet_sql_report/sheet2sql.py
+++ sheet_sql_report/sheet2sql.py
@@ -9,39 +9,59 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 class SheetToSQL():
-
-    def __init__(self, google_cloud_credentials, sql_connection):
+    ### SHOULD THE WHOLE THING JUST FULLY INHERIT FROM GSPREAD?
+    def __init__(self, google_cloud_credentials=None, sql_connection=None):
         """Class to sync a table in a SQL database with data in a Google Sheet
 
         Attributes:
             google_cloud_credentials (JSON file) stores our access credentials to google cloud
             sql_connection (String) stores the connection info for the database for use in sql_academy.create_engine().
                                     dialect+driver://username:password@host:port/database
-
-
         """
-        ### TODO: COULD MAKE THE CREDENTIALS OPTIONAL? LET HAVE METHOD TO ADD LATER? (POINTLESS?)
-        ### TODO: REFACTOR TO MAKE SURE
+
         self.gc_credentials = google_cloud_credentials
         self.sql_connection = sql_connection
 
-    ### ADD FUNCTION TO ADD THE CONNECTION DETAILS THEN LET JUST ADD ONE/NEITHER IN INIT
-    ### SHOULD THE WHOLE THING JUST FULLY INHERIT FROM GSPREAD?
+
+
+    def update_google_details(self, google_cloud_credentials):
+        """Method to add or update the google cloud CREDENTIALS
+        Args:
+            - gc_credentials: a JSON file of the access creddentials for the Google API. Need to use the Google Cloud website to set up a project for this and generate the creddentials
+
+        Returns:
+            - None
+        """
+        self.gc_credentials = google_cloud_credentials
+
+
+    def update_sql_connection(self, sql_connection):
+        """Method to add or update the SQL connection info
+        Args:
+            - sql_connection (String) stores the connection info for the database for use in sql_academy.create_engine().
+                                    dialect+driver://username:password@host:port/database
+        Returns:
+            - None
+        """
+        self.sql_connection = sql_connection
+
 
     def get_gheet_data(self, workbook_name, sheet_name, cell=False):
         """Method to connect to and grab data from the google sheet. If cell supplied grabs just that cell
            otherwise defaults to get all the data in the sheet
 
         Args:
-            - gc_credentials: a JSON file of the access creddentials for the Google API. Need to use the Google Cloud website to set up a project for this and generate the creddentials
             - worksheet_name: the name of our Google Sheet that has the data we want
-            - cell: a sting with the cell to pull the data from e.g. 'A2'
+            - sheet_name: name of the sheet within the workbook we want to access
+            - cell: (OPTIONAL)a sting with the cell to pull the data from e.g. 'A2' otherwise pulls whole sheet
 
         Returns:
             - data: if Cell supplied is a string otherwise is a pandas dataframe
-
-
         """
+        if google_cloud_credentials = None:
+            print('ERROR: No Google Credentails Supplied')
+            print('Use update_google_details() function to add them them then try again')
+            break
 
         ### USE SAVED CREDENTIALS TO CONNECT TO THE GOOGLE SHEETS API
         scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
@@ -76,6 +96,11 @@ class SheetToSQL():
             - df_result: a dataframe containing the result of the query
 
         """
+        if sql_connection = None:
+            print('ERROR: No SQL Database Supplied')
+            print('Use update_sql_connection() function to add them them then try again')
+            break
+
         ## read sql method
         engine = create_engine(self.sql_connection)
         df_result = pd.read_sql(query,con=engine)
@@ -85,7 +110,6 @@ class SheetToSQL():
         return df_result
 
     def data_to_sql(self, df, db_schema, db_table):
-        ### TODO: MORE DESCRIPTIVE NAME? LIKE DATA TO SQL?
         """Method to feed the data to mzee
 
         # https://stackoverflow.com/questions/48006551/speeding-up-pandas-dataframe-to-sql-with-fast-executemany-of-pyodbc
@@ -99,12 +123,16 @@ class SheetToSQL():
         Returns:
             - none
         """
+        if sql_connection = None:
+            print('ERROR: No SQL Database Supplied')
+            print('Use update_sql_connection() function to add them them then try again')
+            break
 
         ### CONNECT TO OUR DATA BASE AND REPLACE THE EXISTING SQL TABLE WITH OUR GSHEET DATA
         ### TODO: ALLOW TO APPEND DATA NOT JUST REPLACE
         engine = create_engine(self.sql_connection)
         df.to_sql(db_table, schema=db_schema, con=engine, if_exists='replace', index=False, method="multi")
-        ### TODO: CLOSE THE SQL ENGINE? OR GIVE OPTION TO LEAVE OPEN? MAYBE SHOULD BE OTHER METHODS SO CAN USE SAME ENGINE TWICE
+        ### TODO: CLOSE THE SQL ENGINE? PANDAS PROBABLY DOES OR GIVE OPTION TO LEAVE OPEN? MAYBE SHOULD BE OTHER METHODS SO CAN USE SAME ENGINE TWICE
 
         print("Successfully added {0} records to {1}.{2}".format(len(df), db_schema, db_table))
 
@@ -117,7 +145,11 @@ class SheetToSQL():
             - sheet_name (string): the name of the worksheet to update
             - starting_cell (string): the cell location to input the new data. Defualt at A2
         """
-        ### TODO: REFACTOR TO CONNECT HAVE METHOD TO CONNECT TO THE GSHEET
+        if google_cloud_credentials = None:
+            print('ERROR: No Google Credentails Supplied')
+            print('Use update_google_details() function to add them them then try again')
+            break
+
         ### USE SAVED CREDENTIALS TO CONNECT TO THE GOOGLE SHEETS API
         scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
         credentials = ServiceAccountCredentials.from_json_keyfile_name(self.gc_credentials, scope)
